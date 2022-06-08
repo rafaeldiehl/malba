48e864e4435b5087ea41a32e271346d5adb0ac20
Rafael Juliani Diehl <rafaeljuliani1984@gmail.com>
2022-06-07 21:35:43 -0300

    add form button component

---
+++ frontend/src/components/FormButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="form-button">
+    <button type="submit">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormButton'
+}
+</script>
+
+<style lang="scss" scoped>
+.form-button {
+  button {
+    @apply bg-sky-500 w-full text-center p-4 rounded-lg text-xl text-white my-4;
+  }
+}
+</style>
--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -1,14 +1,62 @@
 <template>
   <div id="sign-in">
-    <h1>Hello, world</h1>
+    <form @submit.prevent="sendCredentials">
+      <form-input
+        type="text"
+        label="nome"
+        name="name"
+        placeholder="Informe seu nome"
+        v-model="credentials.name"
+        required
+      />
+      <form-input
+        type="password"
+        label="senha"
+        name="password"
+        placeholder="Informe sua senha"
+        v-model="credentials.password"
+        required
+      />
+      <form-button>Entrar</form-button>
+    </form>
+    <p>O nome é {{ credentials.name }}</p>
+    <p>A senha é {{ credentials.password }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import FormInput from '@/components/FormInput.vue';
+import FormButton from '@/components/FormButton.vue';
 
 export default {
   name: 'SignInView',
+  components: {
+    FormInput,
+    FormButton
+  },
+  data() {
+    return {
+      credentials: {
+        name: "",
+        password: ""
+      }
+    }
+  },
+  methods: {
+    sendCredentials() {
+      console.log(this.credentials);
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+form {
+  @apply p-5;
+
+  .input-container, .form-button {
+    @apply max-w-lg;
+  }
+}
+</style>
